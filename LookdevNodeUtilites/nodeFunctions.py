22f3bc44ea48f45ead1a8e085895502a161d58c0
btwtill <57750780+btwtill@users.noreply.github.com>
2024-06-16 13:04:54 +0200

    function to create Color Constant connected to all the selected material

---
+++ LookdevNodeUtilites/nodeFunctions.py
@@ -0,0 +1,23 @@
+#function to create a color constant and connect it to any aiSurface shader in a selection
+
+def colorConstantToSelectionSurfaceShader(selectionList, name = "CombinedColorConstant"):
+    #create new color constant
+    newColorConstantNode = cmds.createNode("colorConstant", name = name)
+    
+    #filter selection list to only aiStandard Surface shaders
+    filteredList = []
+    for sel in selectionList:
+        if cmds.nodeType(sel) == "aiStandardSurface":
+            filteredList.append(sel)
+   
+    print(filteredList)
+    
+    #connect color constant to all the surface shaders
+    for shader in filteredList:
+        for channel in "RGB":
+            cmds.connectAttr(f"{newColorConstantNode}.outColor{channel}", f"{shader}.baseColor{channel}")
+        
+
+selection = cmds.ls(sl=True)
+
+colorConstantToSelectionSurfaceShader(selection)
